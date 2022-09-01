458a8293ffe40fca435e949a620c5b0ecdefb9c9
martinjagodic <jagodicmartin1@gmail.com>
2022-09-01 15:00:30 +0200

    update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,16 @@ module.exports = {
   plugins: [
     require('autoprefixer'),
     purgecss({
-      content: ['./layouts/**/*.html', './assets/scripts/*.js'],
+      content: [
+        './layouts/**/*.html',
+        './assets/scripts/**/*.js',
+        './layouts/*.html',
+        './assets/scripts/*.js',
+        '../layouts/**/*.html',
+        '../assets/scripts/**/*.js',
+        '../layouts/*.html',
+        '../assets/scripts/*.js',
+      ],
       safelist: [
         // add classes found only in js
       ],
